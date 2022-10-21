0011471eff04d72521dff38c4a77a83f4528abc2
Matheus Alan <matheusalanpv@outlook.com>
2022-10-21 15:38:18 -0300

    maven

---
+++ project-parent/controller/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"one.digital.innovation","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"one.digital.innovation","c":"App","l":"main(String[])","url":"main(java.lang.String[])"}]
